c250e3c4ac7890030afdc48a4270f0e1e7c31a82
u01@pc-10.(none) <u01@pc-10.(none)>
2019-04-23 16:01:38 +0430

    modifying register and state

--- warranty/models/models.py
+++ warranty/models/models.py
@@ -1,15 +1,49 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+class Register(models.Model):
+    _name = 'warranty.register'
+    _description = 'Register warranty type'
 
-# class warranty(models.Model):
-#     _name = 'warranty.warranty'
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    warranty_type = fields.Selection([('type 1','Check'),
+                            ('type 2','Promissory Note'),
+                            ('type 3','Bank Guarantee')],
+                             string='Warranty Type', default='Check')
+
+    # warranty_state = fields.Selection([('state 1','Preparing'),
+    #                                       ('state 2','Active'),
+    #                                      ('state 3','Expired'),
+    #                                      ('state 4', 'Refunded')],
+    #                                string='Warranty State', default='Preparing')
+
+    warranty_state = fields.Many2one('warranty.state',
+            ondelete='cascade', string='State', required=True)
+
+    date_start = fields.Date(string='Warranty Since:')
+    date_end = fields.Date(string='Warranty Until:')
+
+    customer = fields.Many2one('warranty.customer',
+        ondelete='cascade', string="Customer", required=True)
+
+    release = fields.Boolean(string='Release State', default=False)
+
+
+class State(models.Model):
+    _name = 'warranty.state'
+    _description = 'Defining Warranty State by Users'
+
+    state_name = fields.Char(string="State", required=True)
+    description = fields.Text()
+
+
+
+class Customer(models.Model):
+    _inherit = 'res.partner'
+    _name = 'warranty.customer'
+    _description = 'Customer of contraction'
+
+    warranty_ids = fields.One2many(
+        'warranty.register', 'customer', string="Registered Warranties")
